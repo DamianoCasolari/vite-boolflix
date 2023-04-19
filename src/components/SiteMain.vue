4614da41ee8a8a8f6b44473b0dc3a030454ac1e1
DamianoCasolari <damianocasolari@gmail.com>
2023-04-19 15:14:34 +0200

    add two searches in one click (tv/movie)

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -18,8 +18,10 @@ export default {
 <template>
     <ul>
         <li v-for="movie in store.listMovies">
-            <div class="title">{{ movie.title }}</div>
-            <div class="original_title">{{ movie.original_title }}</div>
+            <div v-if="movie.title" class="title">{{ movie.title }}</div>
+            <div v-if="movie.name" class="title">{{ movie.name }}</div>
+            <div v-if="movie.original_title" class="original_title">{{ movie.original_title }}</div>
+            <div v-if="movie.original_name" class="original_title">{{ movie.original_name }}</div>
             <div class="lenguage">{{ movie.original_language }}</div>
             <div class="vote">{{ movie.vote_average }}</div>
         </li>
--- src/store.js
+++ src/store.js
@@ -3,14 +3,19 @@ import axios from "axios";
 export const store = {
     nameMovie: "",
     listMovies: [],
-    API_URL: "https://api.themoviedb.org/3/search/movie?api_key=70c8282706a34ee4687ab6063a6b0245&language=en-US&page=1&include_adult=false",
+    API_URL: "https://api.themoviedb.org",
+
     searchMovie(url) {
-        axios
-            .get(url + `&query=${this.nameMovie}`)
-            .then(response => {
-                this.listMovies = response.data.results
-                console.log(this.listMovies);
-            })
+        axios.all([
+            axios.get(url + `/3/search/movie?api_key=70c8282706a34ee4687ab6063a6b0245&language=en-US&page=1&include_adult=false&query=${this.nameMovie}`),
+            axios.get(url + `/3/search/tv?api_key=70c8282706a34ee4687ab6063a6b0245&language=en-US&page=1&include_adult=false&query=${this.nameMovie}`)
+        ]).then(response => {
+            const movies = response[0].data.results
+            const tv = response[1].data.results
+            console.log(movies);
+            console.log(tv);
+            this.listMovies = [...movies, ...tv]
+        })
             .catch(err => {
                 console.error(err.message);
             })
