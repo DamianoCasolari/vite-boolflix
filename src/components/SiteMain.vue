bc279b67eb1373e3464c5fae7ee792306014dd9a
DamianoCasolari <damianocasolari@gmail.com>
2023-04-20 16:56:11 +0200

    layout show case movies

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -32,26 +32,31 @@ export default {
 
 <template>
     <main>
-        <ul class="d-flex flex-wrap align-items-center">
-            <li class="col col-12 col-md-6 col-lg-3 col-xxl-2" v-for="movie, index in store.listMovies" @mouseover=""
-                @mouseleave="">
-                <div v-if="movie.title" class="title">{{ movie.title }}</div>
-                <div v-if="movie.name" class="title">{{ movie.name }}</div>
-                <div v-if="movie.original_title" class="original_title">{{ movie.original_title }}</div>
-                <div v-if="movie.original_name" class="original_title">{{ movie.original_name }}</div>
-                <div class="Cover_container" v-if="movie.poster_path">
-                    <img class="cover_movie" :src="store.image_url + movie.poster_path" alt="Cover movie">
-                </div>
-                <div class="language_container">
-                    <div class="flag" v-if="countryFlags[movie.original_language]">
-                        <img class="flags" :src="countryFlags[movie.original_language]" alt="flag_country">
+        <ul class="d-flex flex-wrap align-items-center justify-content-start">
+            <li class="col col-12 col-md-6 col-lg-3 col-xxl-2 d-flex justify-content-center"
+                v-for="movie, index in store.listMovies" @mouseover="" @mouseleave="">
+                <div class="Cover_container position-relative">
+                    <img v-if="movie.poster_path" class="cover_movie" :src="store.image_url + movie.poster_path"
+                        alt="Cover movie">
+                    <img v-else class="cover_movie not_available" src="../assets/img/not_available.png" alt="Cover movie">
+                    <div class="info_container position-absolute top-0 start-0">
+                        <h3 v-if="movie.title" class="title">{{ movie.title }}</h3>
+                        <h3 v-if="movie.name" class="title">{{ movie.name }}</h3>
+                        <h5 v-if="movie.original_title" class="original_title">{{ movie.original_title }}</h5>
+                        <h5 v-if="movie.original_name" class="original_title">{{ movie.original_name }}</h5>
+                        <div class="language_container">
+                            <span class="flag" v-if="countryFlags[movie.original_language]">
+                                <img class="flags" :src="countryFlags[movie.original_language]" alt="flag_country">
+                            </span>
+                            <span class="lenguage">{{ isoLangs[movie.original_language].name }}</span>
+                        </div>
+                        <div class="vote">{{ "Rating " + (movie.vote_average / 2).toFixed(2) }}</div>
+                        <div id="index" class="">
+                            <font-awesome-icon class="text-warning" icon="star" v-for="icon in this.starVote(movie)" />
+                            <font-awesome-icon icon="star" v-for="icon in this.starEmpty(movie)" />
+                        </div>
+
                     </div>
-                    <div class="lenguage">{{ isoLangs[movie.original_language].name }}</div>
-                </div>
-                <div class="vote">{{ "Rating " + (movie.vote_average / 2).toFixed(2) }}</div>
-                <div id="index" class="d-flex star_container justify-content-center">
-                    <font-awesome-icon class="text-warning" icon="star" v-for="icon in this.starVote(movie)" />
-                    <font-awesome-icon icon="star" v-for="icon in this.starEmpty(movie)" />
                 </div>
 
             </li>
